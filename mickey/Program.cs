379ea930fb35a9064bdc09d49e3076baf6560c1f
Patrick Hallisey <hallipr@gmail.com>
2023-05-22 11:11:34 -0700

    Add step adjustment

--- mickey/Program.cs
+++ mickey/Program.cs
@@ -1,26 +1,55 @@
 ﻿using Iot.Device.ServoMotor;
-using System;
 using System.Device.Pwm.Drivers;
-using System.Threading;
-
-Console.WriteLine("Hello PWM!");
 
 using SoftwarePwmChannel pwmChannel = new(21, 50, usePrecisionTimer: true);
 // Example of hardware PWM using chip 0 and channel 21 on a dev board.
 ServoMotor servoMotor = new ServoMotor(pwmChannel);
 servoMotor.Start();  // Enable control signal.
 
-// Move position.  Pulse width argument is in microseconds.
-Console.WriteLine("servoMotor.WritePulseWidth(1000)");
-servoMotor.WritePulseWidth(1000); // 1ms; Approximately 0 degrees.
-Thread.Sleep(3500);
+var stepSize = 100;
+var pulseWidth = 1500; // 1.5ms; Approximately 90 degrees.
+var min = 500;
+var max = 2500;
 
-Console.WriteLine("servoMotor.WritePulseWidth(1500)");
-servoMotor.WritePulseWidth(1500); // 1.5ms; Approximately 90 degrees.
-Thread.Sleep(3500);
 
-Console.WriteLine("servoMotor.WritePulseWidth(2000)");
-servoMotor.WritePulseWidth(2000); // 2ms; Approximately 180 degrees.
-Thread.Sleep(3500);
+Console.WriteLine($"{pulseWidth} us");
+servoMotor.WritePulseWidth(pulseWidth);
+
+while(true)
+{
+    var key = Console.ReadKey(true);
+    while(Console.KeyAvailable)
+    {
+        Console.ReadKey(true);
+    }
+
+    if(key.Key == ConsoleKey.Escape)
+    {
+        break;
+    }
+
+    if(key.Key == ConsoleKey.UpArrow)
+    {
+        pulseWidth = Math.Min(pulseWidth + stepSize, max);
+        Console.WriteLine($"{pulseWidth} us");
+        servoMotor.WritePulseWidth(pulseWidth);
+    }
+    else if(key.Key == ConsoleKey.DownArrow)
+    {
+        pulseWidth = Math.Max(pulseWidth - stepSize, min);
+        Console.WriteLine($"{pulseWidth} us");
+        servoMotor.WritePulseWidth(pulseWidth);
+    }
+    else if(key.Key == ConsoleKey.LeftArrow)
+    {
+        stepSize -= 10;
+        Console.WriteLine($"{stepSize} step");
+    }
+    else if(key.Key == ConsoleKey.RightArrow)
+    {
+        stepSize += 10;
+        Console.WriteLine($"{stepSize} step");
+    }
+}
 
 servoMotor.Stop(); // Disable control signal.
