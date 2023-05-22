3ee8a53a11fc3f5187ec7739081331a6c6738271
Patrick Hallisey <hallipr@gmail.com>
2023-05-22 10:26:38 -0700

    Initial program

---
+++ mickey/Program.cs
@@ -0,0 +1,26 @@
+﻿using Iot.Device.ServoMotor;
+using System;
+using System.Device.Pwm.Drivers;
+using System.Threading;
+
+Console.WriteLine("Hello PWM!");
+
+using SoftwarePwmChannel pwmChannel = new(21, 50, usePrecisionTimer: true);
+// Example of hardware PWM using chip 0 and channel 21 on a dev board.
+ServoMotor servoMotor = new ServoMotor(pwmChannel);
+servoMotor.Start();  // Enable control signal.
+
+// Move position.  Pulse width argument is in microseconds.
+Console.WriteLine("servoMotor.WritePulseWidth(1000)");
+servoMotor.WritePulseWidth(1000); // 1ms; Approximately 0 degrees.
+Thread.Sleep(3500);
+
+Console.WriteLine("servoMotor.WritePulseWidth(1500)");
+servoMotor.WritePulseWidth(1500); // 1.5ms; Approximately 90 degrees.
+Thread.Sleep(3500);
+
+Console.WriteLine("servoMotor.WritePulseWidth(2000)");
+servoMotor.WritePulseWidth(2000); // 2ms; Approximately 180 degrees.
+Thread.Sleep(3500);
+
+servoMotor.Stop(); // Disable control signal.
---
+++ mickey/README.md
@@ -0,0 +1,9 @@
+# Servos
+Operating trave: 180°±1° (500→2500 μsec)
+Left&Right travelling Angle deviation: ≤ 6°
+Centering deviation: ≤ 1°
+Neutral position: 1500 μsec
+Dead band width: 7 μsec
+Rotating direction: Counter Clockwise (500→2500μsec)
+Pulse width range: 500→2500 μsec
+Maximum Physical Angle: About 200° (Manual control)
