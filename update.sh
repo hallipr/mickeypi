27828c67fb4f6f7a0685a89466cf39877b18826c
Patrick Hallisey <pahallis@microsoft.com>
2023-05-23 00:30:59 -0700

    Fix contidionals in shell script

--- update.sh
+++ update.sh
@@ -7,13 +7,11 @@ printf "$(date +%Y-%m-%d_%H:%M:%S) Local commit $commit\n"
 remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
 printf "$(date +%Y-%m-%d_%H:%M:%S) Remote commit $remotecommit\n"
 
-if(localcommit != remotecommit)
-then
+if [ "$localcommit" != "$remotecommit" ]; then
   pid=$(pgrep mickey)
-  if(test -n "$pid")
-  then
+  if (test -n "$pid"); then
     printf "$(date +%Y-%m-%d_%H:%M:%S) Mickey is running. Shutting it down\n"
-    kill $pid
+    kill -15 $pid
   fi
 
   printf "$(date +%Y-%m-%d_%H:%M:%S) Updating...\n"
@@ -22,8 +20,7 @@ then
   chmod +x update.sh
   printf "$(date +%Y-%m-%d_%H:%M:%S) Updated\n"
 
-  if(test -n "$pid")
-  then
+  if (test -n "$pid"); then
     printf "$(date +%Y-%m-%d_%H:%M:%S) Starting Mickey\n"
     /home/hallipr/.dotnet/dotnet run --project /repos/mickeypi/mickey &
   fi
