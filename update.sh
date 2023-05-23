1f6bf7edb06291b929db4a462835cb751a61cf52
Patrick Hallisey <pahallis@microsoft.com>
2023-05-22 23:25:40 -0700

    Only restart mickey if the repo changes

--- update.sh
+++ update.sh
@@ -4,27 +4,28 @@ commit=$(git rev-parse HEAD)
 printf "Local commit $commit\n"
 remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
 printf "Remote commit $remotecommit\n"
-pid=$(pgrep mickey)
 
-if(test -n "$pid")
-then
-    printf "Mickey is running. Shutting it down\n"
-    kill $pid
-fi
 
 if(localcommit != remotecommit)
 then
-    printf "Updating...\n"
-    git pull
-    chmod +x update.sh
-    printf "Updated\n"
-else
-    printf "Up to date\n"
-fi
+  pid=$(pgrep mickey)
+  if(test -n "$pid")
+  then
+    printf "Mickey is running. Shutting it down\n"
+    kill $pid
+  fi
 
-if(test -n "$pid")
-then
+  printf "Updating...\n"
+  git pull
+  chmod +x update.sh
+  printf "Updated\n"
+
+  if(test -n "$pid")
+  then
     printf "Starting Mickey\n"
     /home/hallipr/.dotnet/dotnet run --project /repos/mickeypi/mickey &
+  fi
+else
+    printf "Up to date\n"
 fi
 ```
