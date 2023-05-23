b34e36eb5c9bd91d1bade5afbb40e36efaacf6b9
Patrick Hallisey <pahallis@microsoft.com>
2023-05-22 23:35:57 -0700

    Add dates to logs

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 pushd /repos/mickeypi
 commit=$(git rev-parse HEAD)
-printf "Local commit $commit\n"
+printf "$(date %Y-%m-%d_%H-%M-%S) Local commit $commit\n"
 remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
-printf "Remote commit $remotecommit\n"
+printf "$(date %Y-%m-%d_%H-%M-%S) Remote commit $remotecommit\n"
 
 
 if(localcommit != remotecommit)
@@ -11,21 +11,20 @@ then
   pid=$(pgrep mickey)
   if(test -n "$pid")
   then
-    printf "Mickey is running. Shutting it down\n"
+    printf "$(date %Y-%m-%d_%H-%M-%S) Mickey is running. Shutting it down\n"
     kill $pid
   fi
 
-  printf "Updating...\n"
+  printf "$(date %Y-%m-%d_%H-%M-%S) Updating...\n"
   git pull
   chmod +x update.sh
-  printf "Updated\n"
+  printf "$(date %Y-%m-%d_%H-%M-%S) Updated\n"
 
   if(test -n "$pid")
   then
-    printf "Starting Mickey\n"
+    printf "$(date %Y-%m-%d_%H-%M-%S) Starting Mickey\n"
     /home/hallipr/.dotnet/dotnet run --project /repos/mickeypi/mickey &
   fi
 else
-    printf "Up to date\n"
+    printf "$(date %Y-%m-%d_%H-%M-%S) Up to date\n"
 fi
-```
