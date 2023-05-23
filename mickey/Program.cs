257d51be2157ae4fcc9f63fbe6f3ea1602d6ae55
Patrick Hallisey <pahallis@microsoft.com>
2023-05-22 23:17:46 -0700

    Update program and update.sh

--- mickey/Program.cs
+++ mickey/Program.cs
@@ -2,10 +2,16 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async void Main(string[] args)
     {
         using var head = new MickeyHead();
         head.Start();
+
+        head.SetEyes(0);
+        head.SetMouth(0);
+        head.SetNose(100);
+
+        await Task.Delay(1000);
 
         head.SetEyes(100);
         head.SetMouth(100);
@@ -13,42 +19,10 @@ public class Program
 
         while(true)
         {
-            var key = Console.ReadKey(true);
-            while(Console.KeyAvailable)
-            {
-                Console.ReadKey(true);
-            }
-
-            switch(key.KeyChar)
-            {
-                case 'a':
-                    head.SetMouth(0);
-                    break;
-                case 'q':
-                    head.SetMouth(100);
-                    break;
-                case 's':
-                    head.SetNose(0);
-                    break;
-                case 'w':
-                    head.SetNose(100);
-                    break;
-                case 'd':
-                    head.SetEyes(0);
-                    break;
-                case 'e':
-                    head.SetEyes(100);
-                    break;
-                default:
-                    break;
-            }
-
-            if(key.Key == ConsoleKey.Escape)
-            {
-                break;
-            }
+           await Task.Delay(2000);
+           head.SetEyes(Random.Shared.Next(0, 100));
+           head.SetMouth(Random.Shared.Next(0, 100));
+           head.SetNose(Random.Shared.Next(0, 100));
         }
-
-        head.Stop();
     }
 }
--- update.sh
+++ update.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-/usr/bin/printf "Hello\n\n$SHELL\n\n"
-/usr/bin/printenv
+commit=$(git rev-parse HEAD)
+printf "Local commit $commit\n"
+remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
+printf "Remote commit $remotecommit\n"
+pid=$(pgrep mickey)
+
+if(test -n "$pid")
+then
+    printf "Mickey is running. Shutting it down\n"
+    kill $pid
+fi
+
+if(localcommit != remotecommit)
+then
+    printf "Updating...\n"
+    git pull
+    printf "Updated\n"
+else
+    printf "Up to date\n"
+fi
