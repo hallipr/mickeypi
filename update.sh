659674a5853f21fe92048bbd9903e670f2a42771
Patrick Hallisey <pahallis@microsoft.com>
2023-05-22 23:44:10 -0700

    Fix dates in update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 pushd /repos/mickeypi
 commit=$(git rev-parse HEAD)
-printf "$(date %Y-%m-%d_%H-%M-%S) Local commit $commit\n"
+printf "$(date +%Y-%m-%d_%H:%M:%S) Local commit $commit\n"
 remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
-printf "$(date %Y-%m-%d_%H-%M-%S) Remote commit $remotecommit\n"
+printf "$(date +%Y-%m-%d_%H:%M:%S) Remote commit $remotecommit\n"
 
 
 if(localcommit != remotecommit)
@@ -11,20 +11,20 @@ then
   pid=$(pgrep mickey)
   if(test -n "$pid")
   then
-    printf "$(date %Y-%m-%d_%H-%M-%S) Mickey is running. Shutting it down\n"
+    printf "$(date +%Y-%m-%d_%H:%M:%S) Mickey is running. Shutting it down\n"
     kill $pid
   fi
 
-  printf "$(date %Y-%m-%d_%H-%M-%S) Updating...\n"
+  printf "$(date +%Y-%m-%d_%H:%M:%S) Updating...\n"
   git pull
   chmod +x update.sh
-  printf "$(date %Y-%m-%d_%H-%M-%S) Updated\n"
+  printf "$(date +%Y-%m-%d_%H:%M:%S) Updated\n"
 
   if(test -n "$pid")
   then
-    printf "$(date %Y-%m-%d_%H-%M-%S) Starting Mickey\n"
+    printf "$(date +%Y-%m-%d_%H:%M:%S) Starting Mickey\n"
     /home/hallipr/.dotnet/dotnet run --project /repos/mickeypi/mickey &
   fi
 else
-    printf "$(date %Y-%m-%d_%H-%M-%S) Up to date\n"
+    printf "$(date +%Y-%m-%d_%H:%M:%S) Up to date\n"
 fi
