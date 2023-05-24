375a88d76321a3e1980eb804142425d006e79b9d
Patrick Hallisey <hallipr@gmail.com>
2023-05-24 00:38:02 -0700

    Temp folder name change

---
+++ Mickey.x/Program.cs
@@ -0,0 +1,32 @@
+using MickeyPi.Character;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var characterSettings = new CharacterSettings();
+builder.Configuration.Bind(nameof(CharacterSettings), characterSettings);
+builder.Services.AddSingleton(characterSettings);
+builder.Services.AddSingleton<CharacterHead>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
