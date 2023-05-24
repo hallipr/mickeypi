ef3b3ffb298c5c7b733c6199ed5720f7f5871bc1
Patrick Hallisey <hallipr@gmail.com>
2023-05-24 01:31:12 -0700

    fix tar command for subdirectories

--- Host/update.sh
+++ Host/update.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 printf "Script location: $BASEDIR\n"
-cd $BASEDIR
 latestRelease=$(curl -s https://api.github.com/repos/hallipr/mickeypi/releases/latest | jq .tag_name -r)
+printf "Latest release: $latestRelease\n"
+
+pushd /var/www
+curl -L "https://github.com/hallipr/mickeypi/releases/download/$latestRelease/webapp.tar.gz" -o ./webapp.tar.gz
+rm -rf ./webapp
+tar -xzf webapp.tar.gz -C ./webapp
