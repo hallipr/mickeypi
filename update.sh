49ba42be453047bdc6db4b013a3a56e62551e320
Patrick Hallisey <pahallis@microsoft.com>
2023-05-23 20:31:43 -0700

    Update update.sh to use github releases

--- update.sh
+++ update.sh
@@ -2,29 +2,4 @@
 BASEDIR=$(dirname $0)
 printf "Script location: $BASEDIR\n"
 cd $BASEDIR
-commit=$(git rev-parse HEAD)
-printf "$(date +%Y-%m-%d_%H:%M:%S) Local commit $commit\n"
-remotecommit=$(git ls-remote --heads -q | grep -E "[[:space:]]refs/heads/main" | cut -f1)
-printf "$(date +%Y-%m-%d_%H:%M:%S) Remote commit $remotecommit\n"
-
-if [ "$commit" != "$remotecommit" ]; then
-  pid=$(pgrep mickey)
-  if (test -n "$pid"); then
-    printf "$(date +%Y-%m-%d_%H:%M:%S) Mickey is running. Shutting it down\n"
-    kill -15 $pid
-  fi
-
-  printf "$(date +%Y-%m-%d_%H:%M:%S) Updating...\n"
-  git fetch
-  git reset --hard origin/main
-  chmod +x update.sh
-  printf "$(date +%Y-%m-%d_%H:%M:%S) Updated\n"
-
-  if (test -n "$pid"); then
-    printf "$(date +%Y-%m-%d_%H:%M:%S) Starting Mickey\n"
-    cd "./mickey"
-    "$HOME/.dotnet/dotnet" run &> ../logs/mickey.log & disown
-  fi
-else
-    printf "$(date +%Y-%m-%d_%H:%M:%S) Up to date\n"
-fi
+latestRelease=$(curl -s https://api.github.com/repos/hallipr/mickeypi/releases/latest | jq .tag_name -r)
