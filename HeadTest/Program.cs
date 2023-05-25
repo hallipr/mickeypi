e94fcc64cfe9a92d6095d1afcd132e57f83792c9
Patrick Hallisey <pahallis@microsoft.com>
2023-05-24 23:29:45 -0700

    Add HeadTest

---
+++ HeadTest/Program.cs
@@ -0,0 +1,91 @@
+﻿using Iot.Device.ServoMotor;
+using MickeyPi.Character;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var head = new CharacterHead(new CharacterSettings
+        {
+            Mouth = {
+                Channel = 19,
+                Frequency = 50,
+                MaximumAngle = 100,
+                MinimumPulseWidthMicroseconds = 1200,
+                MaximumPulseWidthMicroseconds = 1950
+            },
+            Nose = {
+                Channel = 20,
+                Frequency = 50,
+                MaximumAngle = 100,
+                MinimumPulseWidthMicroseconds = 1200,
+                MaximumPulseWidthMicroseconds = 1950
+            },
+            Eyelids = {
+                Channel = 21,
+                Frequency = 50,
+                MaximumAngle = 100,
+                MinimumPulseWidthMicroseconds = 1200,
+                MaximumPulseWidthMicroseconds = 1800
+            },
+        });
+
+        head.Start();
+
+        var stepSize = 100;
+
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            switch(key.Key)
+            {
+                case ConsoleKey.A:
+                    head.Mouth.Angle -= stepSize;
+                    Console.WriteLine($"Mouth {head.Mouth.Angle}");
+                    break;
+                case ConsoleKey.Q:
+                    head.Mouth.Angle += stepSize;
+                    Console.WriteLine($"Mouth {head.Mouth.Angle}");
+                    break;
+                case ConsoleKey.S:
+                    head.Nose.Angle -= stepSize;
+                    Console.WriteLine($"Nose {head.Nose.Angle}");
+                    break;
+                case ConsoleKey.W:
+                    head.Nose.Angle += stepSize;
+                    Console.WriteLine($"Nose {head.Nose.Angle}");
+                    break;
+                case ConsoleKey.D:
+                    head.Eyelids.Angle -= stepSize;
+                    Console.WriteLine($"Eyelids {head.Eyelids.Angle}");
+                    break;
+                case ConsoleKey.E:
+                    head.Eyelids.Angle += stepSize;
+                    Console.WriteLine($"Eyelids {head.Eyelids.Angle}");
+                    break;
+                case ConsoleKey.UpArrow:
+                    stepSize = Math.Min(100, stepSize + 10);
+                    Console.WriteLine($"{stepSize} step");
+                    break;
+                case ConsoleKey.DownArrow:
+                    stepSize = Math.Max(0, stepSize - 10);
+                    Console.WriteLine($"{stepSize} step");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        head.Stop(); // Disable control signal.
+    }
+}
--- ServoTest/Program.cs
+++ ServoTest/Program.cs
@@ -1,66 +1,70 @@
 ﻿using Iot.Device.ServoMotor;
 using System.Device.Pwm.Drivers;
 
-int channel;
-while(true)
+public class Program
 {
-    Console.Write("GPIO Channel: ");
-    var channelString = Console.ReadLine();
-    if(int.TryParse(channelString, out channel))
+    public static void Main(string[] args)
     {
-        break;
-    }
-}
+        int channel;
+        while (true)
+        {
+            Console.Write("GPIO Channel: ");
+            var channelString = Console.ReadLine();
+            if (int.TryParse(channelString, out channel))
+            {
+                break;
+            }
+        }
 
-using SoftwarePwmChannel pwmChannel = new(channel, 50, usePrecisionTimer: true);
-ServoMotor servoMotor = new ServoMotor(pwmChannel);
-servoMotor.Start();  // Enable control signal.
+        using SoftwarePwmChannel pwmChannel = new(channel, 50, usePrecisionTimer: true);
+        ServoMotor servoMotor = new ServoMotor(pwmChannel);
+        servoMotor.Start();  // Enable control signal.
 
-var stepSize = 100;
-var pulseWidth = 1500; // 1.5ms; Approximately 90 degrees.
-var min = 500;
-var max = 2500;
+        var stepSize = 100;
+        var pulseWidth = 1500; // 1.5ms; Approximately 90 degrees.
+        var min = 500;
+        var max = 2500;
 
-Console.WriteLine($"{pulseWidth} us");
-servoMotor.WritePulseWidth(pulseWidth);
-
-while(true)
-{
-    var key = Console.ReadKey(true);
-    while(Console.KeyAvailable)
-    {
-        Console.ReadKey(true);
-    }
-
-    if(key.Key == ConsoleKey.Escape)
-    {
-        break;
-    }
-
-    if(key.Key == ConsoleKey.UpArrow)
-    {
-        pulseWidth = Math.Min(pulseWidth + stepSize, max);
         Console.WriteLine($"{pulseWidth} us");
         servoMotor.WritePulseWidth(pulseWidth);
-    }
-    else if(key.Key == ConsoleKey.DownArrow)
-    {
-        pulseWidth = Math.Max(pulseWidth - stepSize, min);
-        Console.WriteLine($"{pulseWidth} us");
-        servoMotor.WritePulseWidth(pulseWidth);
-    }
-    else if(key.Key == ConsoleKey.LeftArrow)
-    {
-        stepSize -= 10;
-        Console.WriteLine($"{stepSize} step");
-    }
-    else if(key.Key == ConsoleKey.RightArrow)
-    {
-        stepSize += 10;
-        Console.WriteLine($"{stepSize} step");
+
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                pulseWidth = Math.Min(pulseWidth + stepSize, max);
+                Console.WriteLine($"{pulseWidth} us");
+                servoMotor.WritePulseWidth(pulseWidth);
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                pulseWidth = Math.Max(pulseWidth - stepSize, min);
+                Console.WriteLine($"{pulseWidth} us");
+                servoMotor.WritePulseWidth(pulseWidth);
+            }
+            else if (key.Key == ConsoleKey.LeftArrow)
+            {
+                stepSize -= 10;
+                Console.WriteLine($"{stepSize} step");
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                stepSize += 10;
+                Console.WriteLine($"{stepSize} step");
+            }
+        }
+
+        servoMotor.Stop(); // Disable control signal.
     }
 }
-
-servoMotor.Stop(); // Disable control signal.
-
-   
